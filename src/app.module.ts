29532e61f454f156e451997efcc448938bf211b7
parhamkashefi <prhmkashefi@gmail.com>
2025-09-29 20:06:29 +0330

    add telegram controller in telegram module file  and set goldservice and silverservice and telegramservice to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TelegramModule } from './telegram/telegram.module';
 import { ConfigModule } from '@nestjs/config';
+import { TelegramController } from './telegram/telegram.controller';
+import { TelegramService } from './telegram/telegram.service';
+import { GoldService } from './telegram/gold.service';
+import { SilverService } from './telegram/silver.service';
+
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TelegramModule,
-  ],
-  controllers: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),],
+  controllers: [TelegramController],
+  providers: [TelegramService, GoldService, SilverService],
 })
 export class AppModule {}
