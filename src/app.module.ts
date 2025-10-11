0c22e1e78f75bef0c208fcd8c6fa34f31a695874
parhamkashefi <prhmkashefi@gmail.com>
2025-10-11 03:47:33 +0330

    refactor app.module.ts: add MongooseModule for MongoDB connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TelegramController } from './telegram/telegram.controller';
 import { TelegramService } from './telegram/telegram.service';
 import { GoldService } from './telegram/gold.service';
 import { SilverService } from './telegram/silver.service';
 
-
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }),],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://mongo:27017/sopranoBot'),
+  ],
   controllers: [TelegramController],
   providers: [TelegramService, GoldService, SilverService],
 })
