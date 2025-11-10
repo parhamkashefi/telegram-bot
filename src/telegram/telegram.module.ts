e53e8fce966ec726078446e0d8ebd719152ac313
parhamkashefi <prhmkashefi@gmail.com>
2025-11-10 03:35:40 +0330

    refactor: restructure TelegramModule imports and improve organization

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TelegramController } from './telegram.controller';
 import { TelegramService } from './telegram.service';
 import { GoldService } from './gold.service';
 import { SilverService } from './silver.service';
+import { Price, PriceSchema } from './schemas/prices.schema';
+
 
 @Module({
   imports: [
-    ConfigModule, 
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([
+      { name: Price.name, schema: PriceSchema }, 
+    ]),
   ],
-  providers: [TelegramService,GoldService,SilverService],
-  
+  controllers: [TelegramController],
+  providers: [TelegramService, GoldService, SilverService],
+  exports: [TelegramService],
 })
-
 export class TelegramModule {}
