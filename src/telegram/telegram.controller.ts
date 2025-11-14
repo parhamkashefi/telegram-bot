f570545d251f94f9acaa475e5bb8ddb0531cbbb5
parhamkashefi <prhmkashefi@gmail.com>
2025-11-15 01:25:24 +0330

    refactor : api get the prices from db insted of live prices

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,27 +1,36 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { GoldService } from './gold.service';
-import { SilverService } from './silver.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Price, PriceDocument } from './schemas/prices.schema';
 
 @ApiTags('Prices')
 @Controller('prices')
 export class TelegramController {
   constructor(
-    private readonly goldService: GoldService,
-    private readonly silverService: SilverService,
+    @InjectModel(Price.name) private readonly priceModel: Model<PriceDocument>,
   ) {}
 
-  @Get('goldPrice')
-  @ApiOperation({ summary: 'Get all gold prices' })
-  @ApiResponse({ status: 200, description: 'Returns all gold prices' })
-  async getAllGoldPrices() {
-    return await this.goldService.getAllGoldPrices();
+  @Get('gold')
+  @ApiOperation({ summary: 'Get last saved gold price' })
+  @ApiResponse({ status: 200, description: 'Returns latest gold price from DB' })
+  async getLatestGoldPrice() {
+    return await this.priceModel
+      .findOne({ productMaterial: 'gold' })
+      .sort({ createdAt: -1 })
+      .lean();
   }
 
-  @Get('silverPrice')
-  @ApiOperation({ summary: 'Get all silver prices' })
-  @ApiResponse({ status: 200, description: 'Returns all silver prices' })
-  async getAllSilverPrices() {
-    return await this.silverService.getAllSilverPrices();
+  @Get('silver')
+  @ApiOperation({ summary: 'Get last saved silver price' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns latest silver price from DB',
+  })
+  async getLatestSilverPrice() {
+    return await this.priceModel
+      .findOne({ productMaterial: 'silver' })
+      .sort({ createdAt: -1 })
+      .lean();
   }
 }
