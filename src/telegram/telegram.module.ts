5bec4ff52efb1d8ab74064d793f85892e43dafec
parhamkashefi <prhmkashefi@gmail.com>
2025-11-21 21:55:17 +0330

    feat: integrate AuthModule into TelegramModule for JWT authentication

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -6,13 +6,14 @@ import { TelegramService } from './telegram.service';
 import { GoldService } from './gold.service';
 import { SilverService } from './silver.service';
 import { Price, PriceSchema } from './schemas/prices.schema';
-
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    AuthModule,
     MongooseModule.forFeature([
-      { name: Price.name, schema: PriceSchema }, 
+      { name: Price.name, schema: PriceSchema },
     ]),
   ],
   controllers: [TelegramController],
