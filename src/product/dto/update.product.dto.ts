8124b89663a6b2581aafe047369589ab99e58642
parhamkashefi <prhmkashefi@gmail.com>
2025-12-02 22:53:28 +0330

    refactor: remove unused imports from UpdateProductDto

--- src/product/dto/update.product.dto.ts
+++ src/product/dto/update.product.dto.ts
@@ -1,6 +1,5 @@
 import { IsNumber, IsOptional, IsBoolean, IsEnum } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { ProductCategory, ProductType } from '../schema/product.schema';
 
 export class UpdateProductDto {
   @ApiPropertyOptional()
